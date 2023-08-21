30858df2e57f915d88aa8bb4839712909619a606
Mentabomber <mentae_mentology@libero.it>
2023-08-21 19:16:37 +0200

    components & axios install

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
-<script >
-
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,24 @@
 <script>
 
+  import axios from 'axios';
+
+  export default {
+    name: 'HelloWorld',
+    mounted (){
+      axios.get('http://localhost:8000/api/api')
+        .then(response => {
+          const data = response.data;
+          console.log(data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
 </script>
 
 <template>
-  <
+  
 </template>
 
 <style>
